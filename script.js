06a9b7fe8b23574c97ca0f0243c22032d8fb5204
AORA <aora.ship@gmail.com>
2026-07-02 18:00:34 -0300

    Meu portfólio

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.nav-link').forEach(link => {
+  link.addEventListener('click', (e) => {
+    const target = document.querySelector(link.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
